57f80bd276d9b27a42a5ee61994c06a560d29503
napolisiqueira <imfelipenapoli@gmail.com>
2026-08-19 11:16:47 -0300

    Adicionando o lifespan na inicialização

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.core.database import init_db
-from app.routers import health
+from app.routers import exercicio, health
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_db()
     yield
 
-app = FastAPI(title="Hybrid Athlete App")
+app = FastAPI(lifespan=lifespan, title="Hybrid Athlete App")
+app.include_router(health.router)
+app.include_router(exercicio.router)
 
 @app.get("/")
 def read_root():
