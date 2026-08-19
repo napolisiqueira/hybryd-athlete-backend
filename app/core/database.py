4a957de67fef53bf0b67dd835b277864a646bab8
napolisiqueira <imfelipenapoli@gmail.com>
2026-08-19 11:15:57 -0300

    Arrumando bug de obsolescencia/descontinuidade do AsyncIOMotorClient, trocando por pymongo.AsyncMongoClient e inicializando de fato o MongoDB

--- app/core/database.py
+++ app/core/database.py
@@ -1,11 +1,18 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+from pymongo import AsyncMongoClient
 from app.core.config import settings
+from app.models.exercicios import Exercicio
 
-client = AsyncIOMotorClient(settings.mongodb_url)
+client = AsyncMongoClient(settings.mongodb_url)
+
+async def init_db():
+    database = await db_connection()
+    await init_beanie(database, document_models=[Exercicio])
+    print("Conectado ao MongoDB", database.name)
+
 
 async def db_connection():
     database = client[settings.database_name]
     return database
 
-async def init_db():
-    print("Conectado ao MongoDB:", settings.database_name)
+     
